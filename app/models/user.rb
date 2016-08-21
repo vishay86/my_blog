9f5b7dfc9fcc49e1a76ce653883c8f241ac2509a
Vishay Bhatia <vishaybhatia@addictivegifts.com>
2016-08-21 16:09:44 +0400

    add assoc of users and articles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,15 @@
 class User < ActiveRecord::Base
-validates :username, presence: true, 
-          uniqueness: {case_sensitive: false}, 
-          length: {minimum: 3, maximum: 25}
+  has_many :articles
+  before_save{self.email = email.downcase}
 
-VALID_EMAIL_REGEX =  /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :username, presence: true, 
+            uniqueness: {case_sensitive: false}, 
+            length: {minimum: 3, maximum: 25}
 
-validates :email, presence: true,
-          uniqueness: {case_sensitive: false},
-          length: {maximum: 100},
-          format: {with: VALID_EMAIL_REGEX}
+  VALID_EMAIL_REGEX =  /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :email, presence: true,
+            uniqueness: {case_sensitive: false},
+            length: {maximum: 100},
+            format: {with: VALID_EMAIL_REGEX}
 end
