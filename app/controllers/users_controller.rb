585917757c33a09969aa1e5003c0f27ca806e29c
Vishay Bhatia <vishaybhatia@addictivegifts.com>
2016-08-21 22:57:41 +0400

    Added edit update functionality and form partial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 
+  before_action :set_user, only: [:edit, :update]
+
   def new
     @user = User.new
   end
@@ -15,9 +17,25 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @user.update(users_params)
+      flash[:success] = 'User info successfully updated'
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
+
   private
 
   def users_params
     params.require(:user).permit(:username , :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
