abb280d513c6957bb1599faf8930aeb8789d7f89
Vishay Bhatia <vishaybhatia@addictivegifts.com>
2016-08-27 18:08:24 +0400

    added article and category assoc

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,6 @@
 class Category < ActiveRecord::Base
+  has_many :articles_categories
+  has_many :articles, through: :articles_categories
+
   validates :name, presence: true, uniqueness: true , length: {minimum: 3 , maximum: 25}
 end
