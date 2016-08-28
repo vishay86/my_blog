d4c73f922b4becda9acc6d174ace7309b1ce3c85
Vishay Bhatia <vishaybhatia@addictivegifts.com>
2016-08-28 15:08:24 +0400

    updated all views with categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ActiveRecord::Base
-  has_many :articles_categories
-  has_many :articles, through: :articles_categories
+  has_many :article_categories
+  has_many :articles, through: :article_categories
 
   validates :name, presence: true, uniqueness: true , length: {minimum: 3 , maximum: 25}
 end
